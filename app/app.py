823ad0141a194c9a021ad96011ebbafa389f9bcd
Giuseppe leonardi <giuseppeleonardi@MinidiGiuseppe.station>
2025-11-07 02:07:30 +0100

    Aggiunta di Flask+preparazione per k8s

--- app/app.py
+++ app/app.py
@@ -1,11 +1,24 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
 def calculate_discount(price, discount):
     discounted_price = price - (price * discount)
     return discounted_price
 
+@app.route("/discount", methods=["GET"])
+def discount():
+    try:
+        price = float(request.args.get("price"))
+        discount = float(request.args.get("discount"))
+        result = calculate_discount(price, discount)
+        return jsonify({
+            "price": price,
+            "discount": discount,
+            "discounted_price": result
+        })
+    except (TypeError, ValueError):
+        return jsonify({"error": "Invalid parameters"}), 400
+
 if __name__ == "__main__":
-    price = 200
-    discount = 0.15
-    result = calculate_discount(price, discount)
-    print(f"Prezzo originale:{price}")
-    print(f"Sconto: {discount * 100}%")
-    print(f"Prezzo scontato: {result}")
+    app.run(host="0.0.0.0", port=5555)
